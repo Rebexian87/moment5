12e0a3dc15b74d18818a523f7d83137745a60009
Rebecca Smith <rebecca.smith@hotmail.se>
2025-04-25 19:09:41 +0200

    skapt en html-sida för kartor där jag lagt in en karta med hjälp av leaflet. Lagt in kod för detta i scss och jsfil.

--- src/js/app.js
+++ src/js/app.js
@@ -153,15 +153,12 @@ console.table(programs);
     },
     options: {
         scales: {
-          y: {
-            beginAtZero: true
-            
-  
-          }, 
-  
-          x: {
-            max:5
-          }
+
+          
+     
+        
+
+       
         }
   
        
@@ -305,3 +302,13 @@ function sortData3 (){ //Funktion som sorterar tabell i progressionsordning
 
 
 */
+
+
+
+
+
+let map = L.map('map').setView([51.505, -0.09], 10);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
