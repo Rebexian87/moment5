414a34c9abd1b628c935cf2878d9028048b36872
Rebecca Smith <rebecca.smith@hotmail.se>
2025-06-07 19:58:59 +0200

    styling och grid uppdaterad. En del  jsdocs tillagt. gitignore uppdaterad

--- src/js/app2.js
+++ src/js/app2.js
@@ -1,35 +1,48 @@
 
+"use strict"
 
 
+  
 
-  let map = L.map('map').setView([51.505, -0.09], 10);
- L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+let map = L.map('map').setView([51.505, -0.09], 10);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 
 
 
  window.onload = init;
- function init () {
-  searchbtnEl.addEventListener("click", getLocation)  //När sidan startar så anropas funktionen getLocation
+
+ 
+
+   /**Funktion som kallar annan funktion när hemsidan laddas
+    * 
+    */
+function init () {
+ searchbtnEl.addEventListener("click", getLocation)  //När sidan startar så anropas funktionen getLocation
 }
 
 
 
 
-let searchEl=document.querySelector("#testar");
-let searchbtnEl=document.getElementById("testar-btn")
+let searchEl=document.querySelector("#input");
+let searchbtnEl=document.getElementById("search-btn")
 
 
 let location=[];
-
+/**
+ * @async
+ * @function getLocation
+ * @param {string} url - The URL to download from.
+ * @throws {Error} -Om vi inte får någon data
+ */
 async function getLocation () { //Funktion som hämtar data med hjälp av ajaxanrop. Try/Catch Async/Await´
 
 
-  let place=searchEl.value
+ let place=searchEl.value
   
 
  try {const response = await fetch (`https://nominatim.openstreetmap.org/search?addressdetails=1&q=${place}&format=jsonv2&limit=1`)
@@ -48,34 +61,32 @@ catch (error){console.error(error)}
 
 
 
-
+/**
+ * @function displayPlace -Funktion som sätter markör och ändrar karta till platsen som man söker efter
+ * @param {any[]} data 
+ */
 function displayPlace(data) {
 
-
-
-
-
+  /**
+   * Latitude av en plats
+   * @type {number}
+   */
   let latitude = data[0].lat  //["lat"];
   console.log(latitude);
   
+  /**
+   * Longitude av en plats
+   * @type {number}
+   */
   let longitude = data[0].lon  //["lat"];
   console.log(longitude)
-
-  map.flyTo(new L.LatLng(latitude, longitude), 10);
-
-
-
-
-
-  let marker = L.marker([latitude, longitude]).addTo(map);
-
-
-
-
   
 
+  map.flyTo(new L.LatLng(latitude, longitude), 10);
+  /**
+   * Markerar ut en plats på en karta
+   * @type {any}
+   */
+  let marker = L.marker([latitude, longitude]).addTo(map);
 };  
-
-
-
 
